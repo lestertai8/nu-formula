026b68212523bc1d7b23df0a0bac78faf2a1471c
aadityarachmat <adityarachmat2026@u.northwestern.edu>
2023-05-24 16:57:56 -0500

    cleaned up about page

---
+++ src/components/VerticalSwitchBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default VerticalSwitchBar = () => {
+  return (
+    <div>
+      <SwitchButton />
+      <SwitchButton />
+    </div>
+  );
+};
+
+const SwitchButton = () => {
+  return (
+    <div>
+      <text>Text</text>
+    </div>
+  );
+};
