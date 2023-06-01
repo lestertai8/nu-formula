408078e6d27a6060c43eaeac130bb611fa84b5a3
aadityarachmat <adityarachmat2026@u.northwestern.edu>
2023-05-31 19:21:24 -0500

    removed carousel

---
+++ src/components/ContactSection.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DarkSection from "./DarkSection";
+
+export default function ContactSection() {
+  <DarkSection>
+    <text>ContactSection</text>
+  </DarkSection>;
+}
--- src/pages/TestingPage.js
+++ src/pages/TestingPage.js
@@ -1,17 +1,14 @@
 import React from "react";
 import HomepageHeaderSection from "../components/HomepageHeaderSection";
 import InANutshellSection from "../components/InANutshellSection";
-import Carousel from "../components/Carousel";
 import OurTeamsSection from "../components/OurTeamsSection";
 
 function TestingPage() {
   return (
     <div>
-      <text>testing page</text>
       <HomepageHeaderSection />
       <InANutshellSection />
       <OurTeamsSection />
-      <Carousel />
     </div>
   );
 }
