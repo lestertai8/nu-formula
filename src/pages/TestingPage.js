db3fc7b565840d0e397caf01bf6c2a3b29798f71
aadityarachmat <adityarachmat2026@u.northwestern.edu>
2023-05-31 19:40:35 -0500

    Added carousel back

--- src/pages/TestingPage.js
+++ src/pages/TestingPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import HomepageHeaderSection from "../components/HomepageHeaderSection";
 import InANutshellSection from "../components/InANutshellSection";
 import OurTeamsSection from "../components/OurTeamsSection";
+import Carousel from "../components/Carousel";
 
 function TestingPage() {
   return (
     <div>
       <HomepageHeaderSection />
       <InANutshellSection />
+      <Carousel />
       <OurTeamsSection />
     </div>
   );
